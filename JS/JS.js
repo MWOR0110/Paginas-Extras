96ea795a57dade35518e3cc2e2a56dcfe28daf69
MWOR0110 <oliveiramisael20@gmail.con>
2023-02-09 19:24:13 -0300

    começo

---
+++ JS/JS.js
@@ -0,0 +1,5 @@
+window.alert("Para prosseguir deve colocar o seu ano de nascimento")
+var n1 = Number.parseFloat(window.prompt("Qual o ano do seu nascimento?"))
+var result = 2023 - n1
+var resultado = result > 18 ? window.alert('Pode continuar') : window.alert('Não pode continuar');
+
---
+++ JS/Par.js
@@ -0,0 +1,3 @@
+var numero = prompt("Digite um numero");
+var resultado = numero % 2 == 0 ? 'Par' : 'Impar';
+alert(numero + ' é ' + resultado);
